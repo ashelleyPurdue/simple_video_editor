94476924486d5c95a2a21c20f6180d4db219dc66
Alex Shelley <ashelley@purdue.edu>
2017-07-22 16:34:44 -0400

    Implemented all the events in MouseDragMonitor

--- VideoEditorWPF/MouseDragMonitor.cs
+++ VideoEditorWPF/MouseDragMonitor.cs
@@ -23,6 +23,7 @@ namespace VideoEditorWPF
         private UIElement elementWatched;
 
         private bool isDragging = false;
+        private Point prevMousePos;
 
         public MouseDragMonitor(UIElement elementWatched)
         {
@@ -36,17 +37,55 @@ namespace VideoEditorWPF
 
         private void ElementWatched_MouseDown(object sender, MouseButtonEventArgs e)
         {
-            throw new NotImplementedException();
+            //Don't do anything if we're already dragging
+            if (isDragging)
+                return;
+
+            //Start dragging
+            isDragging = true;
+            buttonClicked = e.ChangedButton;
+            prevMousePos = e.GetPosition(elementWatched);
+            Mouse.Capture(elementWatched, CaptureMode.Element);     //Capture the mouse so the user can safely drag the mouse out of the watched object's bounds
+
+            //Send the drag started event
+            DragStarted(new DragEventArgs(buttonClicked, 0, 0));
         }
 
         private void ElementWatched_MouseMove(object sender, MouseEventArgs e)
         {
-            throw new NotImplementedException();
+            //Don't go on if we're not currently dragging
+            if (!isDragging)
+                return;
+
+            //Calculate the delta movement so we can put it in the event args
+            Point currentMousePos = e.GetPosition(elementWatched);
+
+            double deltaX = currentMousePos.X - prevMousePos.X;
+            double deltaY = currentMousePos.Y - prevMousePos.Y;
+
+            //Save the current mouse pos as the previous one, so we can compute the delta again next time
+            prevMousePos = currentMousePos;
+
+            //Fire the drag moved event
+            DragMoved(new DragEventArgs(buttonClicked, deltaX, deltaY));
         }
 
         private void ElementWatched_MouseUp(object sender, MouseButtonEventArgs e)
         {
-            throw new NotImplementedException();
+            //Don't go on if the we're not dragging
+            if (!isDragging)
+                return;
+
+            //Don't go on if the wrong button was released.  We only want to stop dragging if the button that was originally clicked releases.
+            if (e.ChangedButton != buttonClicked)
+                return;
+
+            //Stop dragging
+            isDragging = false;
+            Mouse.Capture(elementWatched, CaptureMode.None);
+
+            //Fire the drag stopped event
+            DragReleased(new DragEventArgs(buttonClicked, 0, 0));
         }
     }
 
@@ -55,5 +94,12 @@ namespace VideoEditorWPF
         public MouseButton button;
         public double deltaX;
         public double deltaY;
+
+        public DragEventArgs(MouseButton button, double deltaX, double deltaY)
+        {
+            this.button = button;
+            this.deltaX = deltaX;
+            this.deltaY = deltaY;
+        }
     }
 }
