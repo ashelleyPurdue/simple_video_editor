5f6fcabb2776ff0ae2b442363d1df6c211141079
Alex Shelley <ashelley@purdue.edu>
2017-09-03 15:27:36 -0400

    Created the project class.

---
+++ VideoEditorWPF/Project.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GleamTech.VideoUltimate;
+
+namespace VideoEditorWPF
+{
+    public class Project
+    {
+        public static Project activeProject;
+
+        public Dictionary<string, VideoFrameReader> importedVideos = new Dictionary<string, VideoFrameReader>();
+    }
+}
